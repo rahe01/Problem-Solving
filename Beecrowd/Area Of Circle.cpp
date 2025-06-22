499c96a5ff79810b38c66990799c19762d354ea0
Rahat Ahmed Rahe <raheahmed009@email.com>
2025-06-22 13:32:48 +0600

    2 code

---
+++ Beecrowd/Area Of Circle.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    double x ;
+
+    cin >> x;
+    const double PI = 3.14159;
+    double area = PI*x*x;
+    cout << fixed << setprecision(4);
+    cout<<"A=" << area<<endl;
+    
+
+    return 0;
+}
---
+++ Beecrowd/Simple Sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int x, y;
+    cin >> x>>y;
+
+    int res = x+y;
+
+    cout << "SOMA = "<<res<<endl;
+
+    
+
+    return 0;
+}
